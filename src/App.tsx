68f7e1cde6cf60a87444dd19892312dbf1bdd177
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-04-14 16:42:07 +0300

    added changeFilter and toggleMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {PlannerItem} from './PlannerItem.tsx';
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 import {useState} from 'react';
 
 export type Menu = {
@@ -9,28 +9,44 @@ export type Menu = {
     isDone: boolean
 }
 
+export type FilterValue = 'Total calories' | 'Proteins' | 'Fats' | 'Carbs'
+
 
 export const App = () => {
 
-    const [menuPlanner, setMenuPlanner] = useState<Menu[]> ([
+    const [menuPlanner, setMenuPlanner] = useState<Menu[]>([
         {id: uuidv4(), title: 'Apple', isDone: true},
         {id: uuidv4(), title: 'Pear', isDone: false},
         {id: uuidv4(), title: 'Plum', isDone: true},
         {id: uuidv4(), title: 'Banana', isDone: true},
         {id: uuidv4(), title: 'Orange', isDone: true}
     ])
+    const [filter, setFilter] = useState<FilterValue>('Total calories')
 
-
+    const changeFilter = () => {
+        setFilter(filter)
+    }
     const data = new Date().toLocaleDateString()
 
     const deleteTask = (taskId: string) => {
         setMenuPlanner(prevMenu => prevMenu.filter(m => m.id !== taskId))
     }
 
+    const toggleTask = (id: string) => {
+        setMenuPlanner(prevMenu => prevMenu.map(el =>
+            el.id === id ? {...el, isDone: !el.isDone} : el))
+    }
+
 
     return (
-        <div className="app" >
-            <PlannerItem title='What to eat (morning)' menu={menuPlanner} date={data} deleteTask={deleteTask} />
+        <div className="app">
+            <PlannerItem title='What to eat (morning)'
+                         menu={menuPlanner}
+                         date={data}
+                         deleteTask={deleteTask}
+                         changeFilter={changeFilter}
+                         toggleTask={toggleTask}
+            />
 
         </div>
     )
--- src/PlannerItem.tsx
+++ src/PlannerItem.tsx
@@ -1,16 +1,21 @@
-import {Task} from './App.tsx';
+import {FilterValue, Menu} from './App.tsx';
 import s from './PlannerItem.module.css'
 import {Buttons} from './Buttons.tsx';
 
 type Props = {
     title: string
-    menu: Task[]
+    menu: Menu[]
     date?: string
     deleteTask: (taskId: string) => void
+    changeFilter: (filter: FilterValue) => void
+    toggleTask: (taskId: string) => void
 }
 
-export const PlannerItem = ( {title, menu, date, deleteTask }: Props) => {
+export const PlannerItem = ( {title, menu, date, deleteTask, toggleTask }: Props) => {
 
+    const onChangeHadler = (taskId: string) => {
+        toggleTask(taskId); // Вызываем функцию из пропсов
+    };
 
     return (
         <div className={s.card}>
@@ -25,7 +30,7 @@ export const PlannerItem = ( {title, menu, date, deleteTask }: Props) => {
                 {menu.map(m => {
                     return (
                         <li key={m.id} >
-                            <input type="checkbox" checked={m.isDone}/>
+                            <input type="checkbox" checked={m.isDone} onChange={() => onChangeHadler(m.id)}/>
                             <span>{m.title}</span>
                             <Buttons title={'х'} onClick={() => deleteTask(m.id)} />
                         </li>
