4ecb88692233f65b0c60448f3572b220782eb836
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-04-23 18:47:28 +0300

    create todolistsReducer and initialiState and type Actions

--- src/model/todolist-reducers.ts
+++ src/model/todolist-reducers.ts
@@ -2,6 +2,11 @@ import {Todolist} from '../App.tsx';
 
 const initialState: Todolist[] = []
 
-export const todolistsReducer = (state: Todolist[] = initialState, action: any): Todolist[] => {
+export const todolistsReducer = (state: Todolist[] = initialState, action: Actions): Todolist[] => {
 
+}
+
+type Actions = {
+    type: string
+    payload: any
 }
